a1caab1cb1e5bc8f0e068a0059b21a697c19d41d
JanofarJahan <janofarjahan2000@gmail.com>
2024-11-07 16:57:33 +0530

    Setup deployment for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Layout from './components/common/Layout';
-import Dashboard from './components/Dashboard';
-import PatientInfo from './components/PatientInfo';
+import logo from "./logo.svg";
+import "./App.css";
+import Layout from "./components/common/Layout";
+import Dashboard from "./components/Dashboard";
+import PatientInfo from "./components/PatientInfo";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
       <Layout>
-      <Router>
-      
-        {/* Define routes here */}
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/:12" element={<PatientInfo />} />
-        </Routes>
-      
-    </Router>
-        {/* <Dashboard/>
-        <PatientInfo/> */}
+        <Router basename="/react-patient-dashboard">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/:12" element={<PatientInfo />} />
+          </Routes>
+        </Router>
       </Layout>
     </div>
   );
